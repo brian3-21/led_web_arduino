e08d88135d6fe316168916da1eed014e10cdf00d
Brian3-21 <brianperezdota@gmail.com>
2024-04-27 12:59:28 -0400

    ok commit

--- serverPy/server.py
+++ serverPy/server.py
@@ -1,28 +1,54 @@
 import asyncio
 import websockets
+import serial
+import time
+
+arduino = serial.Serial(port='COM3', baudrate=9600, timeout=.1)
+
+
+PORT = 8765
+
+# Función para escribir y leer datos desde el Arduino
+def sendArduino(x):
+    arduino.write(bytes(x, 'utf-8'))  # Enviar datos al Arduino
+    time.sleep(0.05)  # Esperar un breve periodo de tiempo
+
+def sendData(data):
+    if data == "P":
+        print("Se ha prendido")
+        sendArduino(data)
+    elif data == "N":
+        print("Se ha apagado")
+        sendArduino(data)
+
+    return data
+
 
 class WebSocketServer:
     def __init__(self):
         self.data = None # Inicializa el atributo de instancia para el estado del LED
 
     async def hello(self, websocket):
-        recived = await websocket.recv()
-        print(f"<<< {recived}")
+        try:
+            
+            recived = await websocket.recv()
+            print(f"<<< {recived}")
+            self.data = sendData(recived)
+            await websocket.send(self.data)
+            print(f"<<< El mensaje enviado es: {self.data}")
+        except websockets.exceptions.ConnectionClosed as errorCC:
+            print("Se ha desconectado alguien")
 
-        if recived == 'P':
-            print("Has encendido el LED")
-            self.data = 'P'
-        elif recived == 'N':
-            print("Has apagado el led")
-            self.data = 'N'
 
-        await websocket.send(self.data)
-        print(f"<<< El mensaje enviado es: {self.data}")
-        
+
     async def main(self):
-        async with websockets.serve(self.hello, "localhost", 8765):
+        async with websockets.serve(self.hello, "localhost", PORT):
             await asyncio.Future() # run forever
+    
+    
 
 if __name__ == "__main__":
     server = WebSocketServer()
+    print("Corriendo servidor en el puerto: " + str(PORT))
     asyncio.run(server.main())
+
